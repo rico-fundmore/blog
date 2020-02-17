5d1526ca6de9f25c886596c045da10ef981a28c3
rico <rico@fundmore.ai>
2020-02-16 20:41:26 -0500

    Finished single, added stuff to index page

--- user/themes/bootstrap/js/main.js
+++ user/themes/bootstrap/js/main.js
@@ -1,13 +1,39 @@
-  if( window.innerWidth > 768){
-  // Also can pass in optional settings block
-  var rellax = new Rellax('.avlx', {
-    speed: -2,
-    breakpoints: [576, 768, 1201],
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
+(
+    function(){
 
-}
+        if( window.innerWidth > 768){
+            // Also can pass in optional settings block
+            var rellax = new Rellax('.avlx', {
+              speed: -2,
+              breakpoints: [576, 768, 1201],
+              center: false,
+              wrapper: null,
+              round: true,
+              vertical: true,
+              horizontal: false
+            });
+          
+          }
+
+
+ 
+ 
+
+          
+
+          const burgerDiv = document.getElementById('burgerMenu');
+          let activeState = false;
+
+          burgerDiv.addEventListener('click', function(){
+            if(!activeState){
+                burgerDiv.classList.add('activated');
+            }else{
+                burgerDiv.classList.remove('activated');
+            }
+            activeState = !activeState;
+          })
+    }
+)();
+
+
+
